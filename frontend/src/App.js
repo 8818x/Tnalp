c212410ddd203c900108933f62adcbf79d5f1b2d
Amane <vcajdecalul@gmail.com>
2023-03-20 10:05:53 +0700

    Add Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
-
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from './screen/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to="/">Tnalp</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Tnalp</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />}></Route>
-            <Route path="/" element={<HomeScreen />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />}></Route>
+              <Route path="/" element={<HomeScreen />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>© 2023 Tnalp. All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
