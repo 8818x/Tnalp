b96cd3c3db9695592df7f864b552a481b3f41f64
Amane <vcajdecalul@gmail.com>
2023-03-21 14:59:32 +0700

    Add Rating Stars and Reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import HomeScreen from "./screen/HomeScreen";
-import ProductScreen from './screen/ProductScreen';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import { useEffect, useReducer, useState } from "react";
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../components/Product";
 
 
 const reducer = (state, action) => {
@@ -48,20 +51,14 @@ function HomeScreen() {
                 ) : error ? (
                     <div>{error}</div>
                 ) : (
-                    products.map((product) => (
-                        <div className="product" key={product.slug}>
-                            <Link to={`/product/${product.slug}`}>
-                                <img src={product.image} alt={product.name} />
-                            </Link>
-                            <div className="product-info">
-                                <Link to={`/product/${product.slug}`}>
-                                    <p>{product.name}</p>
-                                </Link>
-                                <p><strong>฿{product.price}</strong></p>
-                                <button>Add to cart</button>
-                            </div>
-                        </div>
-                    )))}
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product.slug} sm={6} md={3} lg={3} className="mb-3" >
+                                <Product product={product}></Product>
+                            </Col>
+                        ))}
+                    </Row>
+                )}
             </div>
         </div>
 
