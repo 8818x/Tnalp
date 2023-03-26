c03c69d3ba64768bbcf065ce2ad44f9e1352c360
Amane <vcajdecalul@gmail.com>
2023-03-26 21:02:07 +0700

    Fix API

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
-        slug: { type: String, required: true, unique: true },
-        image: { type: String, unique: true },
-        brand: { type: String, unique: true },
-        category: { type: String, unique: true },
-        description: { type: String, required: true },
-        price: { type: Number, required: true },
-        countInStock: { type: Number, required: true },
-        rating: { type: Number, required: true },
-        numReviews: { type: Number, required: true },
+        name: { type: String, unique: true },
+        slug: { type: String, unique: true },
+        image: { type: String },
+        brand: { type: String },
+        category: { type: String },
+        description: { type: String },
+        price: { type: Number },
+        countInStock: { type: Number },
+        rating: { type: Number },
+        numReviews: { type: Number },
     },
     {
         timestamps: true,
@@ -19,4 +19,5 @@ const productSchema = new mongoose.Schema(
 );
 
 const Product = mongoose.model('Product', productSchema);
+
 export default Product;
--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,23 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log('Database Connected');
-})
-.catch((err) => {
-    console.log(err.message);
-});
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Database Connected');
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
 
 const app = express();
-
 app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter)
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
+app.use('/api/products', productRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9002;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
 })
