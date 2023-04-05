398cdf4981860b890bd854d35128ee9f1e01fc1c
Amane <vcajdecalul@gmail.com>
2023-04-05 21:01:34 +0700

    Update User and Password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
   }
   return (
     <BrowserRouter>
@@ -83,6 +85,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />}></Route>
               <Route path='/signin' element={<SigninScreen />}></Route>
               <Route path='/signup' element={<SignupScreen />}></Route>
+              <Route path='/profile' element={<ProfileScreen/>}></Route>
               <Route path='/shipping' element={<ShippingAddressScreen />}></Route>
               <Route path='/payment' element={<PaymentMethodScreen />}></Route>
               <Route path='/placeorder' element={<PlaceOrderScreen />}></Route>
